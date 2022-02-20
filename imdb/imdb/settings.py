9b13f6b97dd3a88a37f288bf9c901e2214704e00
OscarHagman <oscar.leslie.hagman@gmail.com>
2022-02-20 10:11:20 +0100

    basic version complete

--- imdb/imdb/settings.py
+++ imdb/imdb/settings.py
@@ -14,7 +14,7 @@ NEWSPIDER_MODULE = 'imdb.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'imdb (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:97.0) Gecko/20100101 Firefox/97.0' #'imdb (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
--- imdb/imdb/spiders/scrape_title.py
+++ imdb/imdb/spiders/scrape_title.py
@@ -16,18 +16,43 @@ class ScrapeTitleSpider(scrapy.Spider):
         yield scrapy.Request(url=URL, callback=self.parse)
 
     def parse(self, response):
-        title_page = response.xpath("//div[@id='ipc-wrap-background-id']")
+        title_page = response.xpath("//main[@role='main']")
 
-        name = title_page.xpath("//h1[contains(@class, 'TitleHeader__TitleText-sc-1wu6n3d-0 dxSWFG')]/text()").get()
+        # TITLE HEADER SECTION
+        title_name = title_page.css(".TitleHeader__TitleText-sc-1wu6n3d-0.dxSWFG::text").get()
         
-        rating_div = title_page.xpath("//div[contains(@class, 'AggregateRatingButton__ContentWrap-sc-1ll29m0-0')]")
-        rating = rating_div.xpath("//span[contains(@class, 'AggregateRatingButton__RatingScore-sc-1ll29m0-1 iTLWoV')]/text()").get()
+        # RATING SECTION IN TITLE HEADER
+        rating_div = title_page.css("div.AggregateRatingButton__ContentWrap-sc-1ll29m0-0.hmJkIS")
+        rating = rating_div.css("span.AggregateRatingButton__RatingScore-sc-1ll29m0-1.iTLWoV::text").get()
+        num_of_votes = rating_div.css("div.AggregateRatingButton__TotalRatingAmount-sc-1ll29m0-3.jkCVKJ::text").get()
 
-        tech_spec_div = scrapy.Selector(title_page).css('.ipc-metadata-list ipc-metadata-list--dividers-none ipc-metadata-list--compact ipc-metadata-list--base')
+        # MEDIA SECTION
+            # Get title image
+            # Get title trailer
+
+        # STORYLINE SECTION
+        storyline_section = title_page.xpath("//section[@data-testid='Storyline']")
+        storyline_text = storyline_section.xpath("//div[@data-testid='storyline-plot-summary']//div/div/text()").get()
+        # GENRES SECTION IN STORYLINE
+        genres_selector_list = storyline_section.xpath("//li[@data-testid='storyline-genres']//ul[@role='presentation']//li")
+        genres = []
+        for genre in genres_selector_list:
+            genres.append(genre.xpath("./a/text()").get())
+        
+        # DETAILS SECTION
+
+        #tech_spec_div = scrapy.Selector(title_page).css('.ipc-metadata-list.ipc-metadata-list--dividers-none.ipc-metadata-list--compact.ipc-metadata-list--base')
+        #runtime = tech_spec_div.xpath("./div[@class='ipc-metadata-list-item__content-container']/text()").get()
+
+        #//main[@role="main"]//div[@class='Storyline__StorylineWrapper-sc-1b58ttw-0 iywpty']//div[@data-testid="storyline-plot-summary"]//div/div/text()
+        # //main[@role="main"] //div[@class="Storyline__StorylineWrapper-sc-1b58ttw-0 iywpty"] //div[@data-testid="storyline-plot-summary"] //div/div/text()
         
 
         return {
             "id": self.ID, 
-            "name": name,
-            "rating": rating
+            "name": title_name,
+            "rating": rating,
+            "number of votes": num_of_votes,
+            "genres": genres,
+            "storyline": storyline_text
         }
